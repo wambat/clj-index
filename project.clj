acb5e7469fb1c21f739fa9980201717d62a2af5f
Mike Konkov <noospheratum@gmail.com>
2015-08-31 16:54:43 +0300

    Version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.ikoblik/clj-index "0.0.2"
+(defproject org.clojars.wambat/clj-index "0.0.1"
   :description "String indexing and matching algorithms"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-swank "1.4.4"]]
   :java-source-paths ["src/main/java"]
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-
   :compile-path "classes"
+
 )
