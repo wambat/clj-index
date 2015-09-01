4c048bc09cf12cbb528aa5de3b1c6f0dd03d94e0
Mike Konkov <noospheratum@gmail.com>
2015-09-01 16:35:08 +0300

    clojure ver. bump

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject org.clojars.wambat/clj-index "0.0.1"
+(defproject org.clojars.wambat/clj-index "0.0.2"
   :description "String indexing and matching algorithms"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-swank "1.4.4"]]
+  :pedantic? :warn
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins []
   :java-source-paths ["src/main/java"]
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
